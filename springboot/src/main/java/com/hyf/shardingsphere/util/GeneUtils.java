91abe5ed9ce2447151e407397ecf08c5d263f5f6
hyfsy <1577975140@qq.com>
2022-09-16 16:07:00 +0800

    gene

--- springboot/src/main/java/com/hyf/shardingsphere/util/GeneUtils.java
+++ springboot/src/main/java/com/hyf/shardingsphere/util/GeneUtils.java
@@ -7,15 +7,26 @@ package com.hyf.shardingsphere.util;
 public class GeneUtils {
 
     public static void main(String[] args) {
-        // long gene = gene("扫东方红", 4);
-        // Long id = SnowflakeUtils.generate(gene);
-        // System.out.println(id);
+        long id = Math.abs("userId".hashCode());
+        long gene = Math.abs("张三".hashCode());
+        int genSize = 5;
 
+        System.out.println(Long.toBinaryString(id) + " -> " + id);
+        System.out.println(Long.toBinaryString(gene) + " -> " + gene);
+
+        long gene2 = gene2(id, gene, genSize);
+        long gene3 = gene3(id, gene, genSize);
+        System.out.println(Long.toBinaryString(gene2) + " -> " + gene2);
+        System.out.println(Long.toBinaryString(gene3) + " -> " + gene3);
     }
 
-    public static long gene(String str, long length) {
-        int sum = str.chars().sum();
-        return sum & (1L << length) - 1L;
+    public static long gene2(long id, long gene, int geneSize) {
+        long geneMask = (1L << geneSize) - 1L;
+        return Math.abs(id - (id & geneMask) + (gene & geneMask));
+    }
+
+    public static long gene3(long id, long gene, int geneSize) {
+        return Math.abs((id << geneSize) + (gene & ((1L << geneSize) - 1)));
     }
 
 }
